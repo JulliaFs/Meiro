442c7a6d53f1a9fc3b3fb8270520b6e1c5b75312
Jullia Fs <jf1915327@gmail.com>
2026-06-27 12:18:38 -0300

    fix: forca atualizacao imediata do service worker do PWA

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,9 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
       },
     }),
   ],
